b8d25a49c10073c1773f491de2f7fa88256f3daf
mhawad <mirnahelmybio@yahoo.com>
2022-03-01 11:29:53 -0500

    Add files via upload

---
+++ Swift4/war-challenge/war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Mirna Helmy on 2/21/22.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
